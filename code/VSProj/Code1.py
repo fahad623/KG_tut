3f7f71e48c0e95a0485494a76392a57d4f0305df
fahad623 <fahad2191@gmail.com>
2014-10-21 22:28:58 -0500

    SVR

--- code/VSProj/Code1.py
+++ code/VSProj/Code1.py
@@ -1,24 +1,23 @@
 import pandas as pd
 import numpy as np
-from sklearn.svm import LinearSVC
+from sklearn import svm
 from sklearn.grid_search import GridSearchCV
-from sklearn.metrics import mean_absolute_error
 
 def cv_optimize(X_train, Y_train, clf):
-    list_C = np.logspace(-4, 3, num=8)
-    list_epsilon = np.logspace(-7, 0, num=8)
-    list_epsilon = [0.1]
+    list_C = np.logspace(-3, 4, num=8)
+    #list_epsilon = np.logspace(-5, 2, num=8)
+    list_epsilon = [0.2]
+    list_C = [1.0]
 
     parameters = {"C": list_C, "epsilon": list_epsilon}
-    gs = GridSearchCV(clf, param_grid = parameters, cv = 10, n_jobs = 8, scoring = 'mean_absolute_error')
+    gs = GridSearchCV(clf, param_grid = parameters, cv = 5, n_jobs = 16, verbose = 5)
     gs.fit(X_train, Y_train)
     print "gs.best_params_ = {0}, gs.best_score_ = {1}".format(gs.best_params_, gs.best_score_)
     return gs.best_estimator_, gs.best_params_, gs.best_score_
 
 def fit_clf(X_train, Y_train):
-    clf = svm.SVR(kernel = 'linear', C = bp['C'], epsilon = bp['epsilon'])
-    clf, bp, bs = cv_optimize(X_train, Y_train)
-    
+    clf = svm.SVR(kernel = 'linear')
+    clf, bp, bs = cv_optimize(X_train, Y_train, clf)    
     clf.fit(X_train, Y_train)
     return clf
 
@@ -29,7 +28,7 @@ if __name__ == '__main__':
     df_train_Y = pd.read_csv("..\\..\\data\\y_train.csv")
     df_test = pd.read_csv("..\\..\\data\\X_test.csv")
 
-    df_output = pd.DataFrame(df_test[['Id']])
+    df_output = pd.DataFrame({'Id': range(1954, 2791)})
 
     print df_train_X.shape
     print df_test.shape
@@ -43,11 +42,16 @@ if __name__ == '__main__':
         Y_train = df_train_Y[colName].values
 
         clf = fit_clf(X_train, Y_train)
-        print clf.score(X_train, Y_train)
+        predicted_train = clf.predict(X_train)
         predicted_test = clf.predict(X_test)
+
+        df_train_Y[colName] = predicted_train
+
         df_output[colName] = predicted_test
 
-    df_output.to_csv("..\\..\\..\\data\\output.csv", index = False)
+    df_output.to_csv("..\\..\\data\\output.csv", index = False)
+
+    df_train_Y.to_csv("..\\..\\data\\y_predict.csv", index = False)
 
 
 
---
+++ code/VSProj/test1.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+
+df1 = pd.DataFrame({'Id': range(1954, 2791)})
+
+print df1.head()
+print df1.tail()
